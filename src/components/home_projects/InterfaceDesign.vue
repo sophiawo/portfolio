ad452abd73c043e3265bc9940235c3f12734f9ff
Sophia Wonka <sophia.wonka@hs-augsburg.de>
2025-01-08 10:30:45 +0100

    fixed interfacedesign html + css

--- src/components/home_projects/InterfaceDesign.vue
+++ src/components/home_projects/InterfaceDesign.vue
@@ -11,22 +11,22 @@
 </script>
 
 <template>
-    <div id="interfacedesign">
+    <div id="interface_design">
         <div class="leftside_id">
             <img class="interface_img" src="/src/assets/interface_design/sign_up_screen.png" />
             <img class="interface_img" src="/src/assets/interface_design/main_screen.png" />
             <img class="interface_img" src="/src/assets/interface_design/achievements_screen.png" />
         </div>
         <div class="rightside_id">
-            <h3 id="headinginterface">INTERFACEDESIGN</h3>
-            <p class="interfacetext"> Through an university class I got in contact with app interface desdign. Our
+            <h3 id="heading_interface">INTERFACEDESIGN</h3>
+            <p class="interface_text"> Through an university class I got in contact with app interface desdign. Our
                 app was a time management tool with fokus on balancing work and free time. The choosen Art Déco style
                 with its symmetrical and balanced elements supports the goal of having a good
                 work-life-balance.
             </p>
-            <p class="interfacetext"> Read more about the process behind by clicking the following button.</p>
-            <div id="buttonFlexboxInterface">
-                <div  id="interfacebutton">
+            <p class="interface_text"> Read more about the process behind by clicking the following button.</p>
+            <div id="button_flexbox_interface">
+                <div  id="interface_button">
                     <Button @click="$router.push('projects')" :buttonTextKey="'read more about interfacedesign'" />
                 </div>
             </div>
