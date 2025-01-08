a26d820fd4a62926e829ae462b64a4479f6528f7
Sophia Wonka <sophia.wonka@hs-augsburg.de>
2025-01-08 10:28:21 +0100

    fixed gd html + css

--- src/components/home_projects/GraphicDesign.vue
+++ src/components/home_projects/GraphicDesign.vue
@@ -11,16 +11,16 @@
 </script>
 
 <template>
-    <div id="graphicdesign">
+    <div id="graphic_design">
         <div class="leftside_gd">
-            <h3 id="headinggraphicdesign">GRAPHIC DESIGN</h3>
-            <p class="designtext"> I had the pleasure of designing the poster and flyer for the summer 2023
+            <h3 id="heading_graphic_design">GRAPHIC DESIGN</h3>
+            <p class="design_text"> I had the pleasure of designing the poster and flyer for the summer 2023
                 production of the So1Theater theater group (whos website I also reworked).
                 I tried to create an outstanding and attention grabing yet simple design that
                 ties in with the theme of the play ('Twelve Angry Men').
             </p>
-            <p class="designtext"> Read more about the process behind by clicking the following button.</p>
-            <div  id="designbutton">
+            <p class="design_text"> Read more about the process behind by clicking the following button.</p>
+            <div  id="design_button">
                 <Button @click="$router.push('projects')" :buttonTextKey="'read more about graphic design'" />
             </div>
         </div>
