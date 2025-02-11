40510bff41e94baeeee8f8a6c9b1d2dfc12d6813
Sophia Wonka <sophia.wonka@hs-augsburg.de>
2025-02-11 20:22:17 +0100

    working on contact form

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -1,32 +1,35 @@
 <script>
+import MyButton from './MyButton.vue';
+
 export default {
+    components: {
+        MyButton,
+    },
+
     name: "ContactForm"
 }
 </script>
 
 <template>
     <div class="contact-form-container" role="form" aria-babelledby="form-title">
-        <h3 id="contact-form-heading">Contact formular</h3>
         <form id="contact-form" aria-describedby="form-descpription">
             <p>Fill out this contact form to contact me directly!</p>
-
-            <div id="contact-form-name-line">
-                <label for="name">Name:</label>
-                <input type="text" id="name" name="name" required aria-required="true" aria-label="name" placeholder="your name">
-
-                <label for="surname">Surname:</label>
-                <input type="text" id="surename" name="surname" required aria-required="true" aria-label="surname" placeholder="your surname">
+            <div id="contact-first-line">
+                <div class="input-section" id="name-section">
+                    <label for="name">Name*</label>
+                    <input type="text" id="name" name="name" required aria-required="true" aria-label="name" placeholder="John">
+                </div>
+                <div class="input-section" id="email-section">
+                    <label for="email">E-Mail*</label>
+                    <input type="email" id="email" name="email" required aria-required="true" aria-label="email" placeholder="john.doe@example.com">
+                </div>
             </div>
-            
-            <label for="email">E-Mail:</label>
-            <input type="email" id="email" name="email" required aria-required="true" aria-label="email" placeholder="your email address">
-
-            <label for="message">Your message:</label>
-            <textarea id="message" name="message" rows="5" required aria-required="true" aria-label="message" placeholder="your message to me"></textarea>
-
-            <button type="submit" aria-label="Send">Send your message</button>
+            <div class="input-section" id="text-section">
+                <label for="message">Your message*</label>
+                <textarea id="message" name="message" rows="5" required aria-required="true" aria-label="message" placeholder="your website is really interessting..."></textarea>
+            </div>
+            <MyButton id="contact-send-button" class="segment-button-black" type="submit" aria-label="send" :buttonTextKey="'contact.sendButton'" />
         </form>
-        <p id="success-message" class="hidden" role="alert">Thank you for your message!</p>
     </div>
 </template>
 
--- src/views/ContactPage.vue
+++ src/views/ContactPage.vue
@@ -11,12 +11,6 @@ export default {
 <template>
     <div class="contact">
         <h2> {{ $t("common.contactCaps") }}</h2>
-        <div id="emailSection">
-            <div id="emailSectionContent" class="contact-line">
-                <span class="material-symbols-outlined contact-icon">mail</span>
-                <a href="mailto:sophia.m.wonka@gmail.com">sophia.m.wonka@gmail.com</a>
-            </div>
-        </div>
         <ContactForm />
     </div>
 </template>
