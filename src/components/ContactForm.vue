abdbada305ff07793927f3b8aabcd7c62edfbdad
Sophia Wonka <sophia.wonka@hs-augsburg.de>
2025-02-17 21:37:45 +0100

    made better sections / cleaned up code

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -6,13 +6,6 @@ export default {
         MyButton,
     },
     name: "ContactForm",
-    methods: {
-        autoGrow(event) {
-            const element = event.target;
-            element.style.height = "auto";
-            element.style.height = element.scrollHeight + "px";
-        }
-    }
 }
 </script>
 
@@ -21,18 +14,53 @@ export default {
         <form id="contact-form" aria-describedby="form-descpription">
             <p>Fill out this contact form to contact me directly!</p>
             <div id="contact-first-line">
+
+                <!--name input field-->
                 <div class="input-section" id="name-section">
                     <label for="name">Name*</label>
-                    <input type="text" id="name" name="name" required aria-required="true" aria-label="name" placeholder="John Doe">
+                    <div class="input-container">
+                        <input type="text" 
+                            id="name" 
+                            name="name" 
+                            required 
+                            aria-required="true" 
+                            pattern="[A-Za-z]+" 
+                            title="Only letters, spaces, and hyphens are allowed. At least 2 characters." 
+                            aria-label="name" 
+                            placeholder="John Doe">
+                        <span class="material-symbols-outlined contact-invalid-icon">close</span>
+                    </div>
+                    <span class="error-message">Please enter a valid name!</span>
                 </div>
+
+                <!--email input field-->
                 <div class="input-section" id="email-section">
                     <label for="email">E-Mail*</label>
-                    <input type="email" id="email" name="email" required aria-required="true" aria-label="email" placeholder="john.doe@example.com">
+                    <div class="input-container">
+                        <input type="email" 
+                            id="email" 
+                            name="email" 
+                            required 
+                            aria-required="true" 
+                            title="Must include a @ symbol."
+                            aria-label="email" 
+                            placeholder="john.doe@example.com">
+                        <span class="material-symbols-outlined contact-invalid-icon">close</span>
+                    </div>
+                    <span class="error-message">Please enter a valid email!</span>
                 </div>
             </div>
+
+            <!--message text field-->
             <div class="input-section" id="text-section">
                 <label for="message">Your message*</label>
-                <textarea @input="autoGrow" id="message" name="message" required aria-required="true" aria-label="message" placeholder="your website is really interessting..."></textarea>
+                <textarea @input="autoGrow" 
+                    id="message" 
+                    name="message" 
+                    required 
+                    aria-required="true" 
+                    aria-label="message" 
+                    placeholder="your website is really interessting..."></textarea>
             </div>
             <div id="contact-last-line">
                 <MyButton id="contact-send-button" class="segment-button-black" type="submit" aria-label="send" :buttonTextKey="'contact.sendButton'" />
