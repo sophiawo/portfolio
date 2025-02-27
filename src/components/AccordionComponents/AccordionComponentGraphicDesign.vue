4cb4c99e0368a1baff44a598f8957ae3c3b784db
Sophia Wonka <sophia.wonka@hs-augsburg.de>
2025-02-27 22:14:20 +0100

    started adding content graphic design accordion

--- src/components/AccordionComponents/AccordionComponentGraphicDesign.vue
+++ src/components/AccordionComponents/AccordionComponentGraphicDesign.vue
@@ -1,6 +1,27 @@
 <script>
+import LightboxImage from '../LightboxImage.vue';
+
+
     export default{
-        name: "AccordionComponentGraphicDesign",
+      components: {
+        LightboxImage
+      },
+
+      data() {
+        return {
+          gdImgClass: "project-gd-picture",
+          tam1: "/src/assets/graphic_design/angry_men1.JPG",
+          tam1Alt: "Lorem",
+          tam2: "/src/assets/graphic_design/angry_men2.JPG",
+          tam2Alt: "Lorem",
+          poster: "/src/assets/graphic_design/theater_poster.png",
+          posterAlt: "Lorem",
+          flyer: "/src/assets/graphic_design/theater_flyer.png",
+          flyerAlt: "Lorem"
+        };
+      },
+
+      name: "AccordionComponentGraphicDesign",
     }
 </script>
 
@@ -11,8 +32,26 @@
         <span class="material-symbols-outlined arrow-icon">keyboard_arrow_right</span>
       </button>
       <div class="content-accordion">
+        <div id="projects-tam-container">
         <h4>{{ $t("projects.graphicdesign.twelveAngryMen") }}</h4>
-        <p>Lorem lorem</p>
+        
+        <div class="tam-content-flexbox">
+          <div class="tam-content-text-container">
+            <p>{{ $t("projects.graphicdesign.tamtext1") }}</p>
+            <p>{{ $t("projects.graphicdesign.tamtext2") }}</p>
+            <p>{{ $t("projects.graphicdesign.tamtext3") }}</p>
+            <p>{{ $t("projects.graphicdesign.tamtext4") }}</p>
+            <p>{{ $t("projects.graphicdesign.tamtext5") }}</p>
+          </div>
+          <LightboxImage :imageClass="gdImgClass" :image="tam1" :altText="tam1Alt" />
+          <LightboxImage :imageClass="gdImgClass" :image="tam2" :altText="tam2Alt" />
+        </div>
+        <div class="tam-content-flexbox">
+          <LightboxImage :imageClass="gdImgClass" :image="poster" :altText="posterAlt" />
+          <LightboxImage :imageClass="gdImgClass" :image="flyer" :altText="flyerAlt" />
+        </div>
+        
+        </div>
         <h4>{{ $t("projects.graphicdesign.communicatingDoors") }}</h4>
         <p>Lorem lorem</p>
       </div>
