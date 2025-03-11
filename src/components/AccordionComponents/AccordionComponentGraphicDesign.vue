f35528eac2a523889c933b8bacd2ef0470442e90
Sophia Wonka <sophiawonka01@gmail.com>
2025-03-11 14:46:56 +0100

    started with communicating doors Abschnitt

--- src/components/AccordionComponents/AccordionComponentGraphicDesign.vue
+++ src/components/AccordionComponents/AccordionComponentGraphicDesign.vue
@@ -10,6 +10,8 @@ import LightboxImage from '../LightboxImage.vue';
       data() {
         return {
           gdImgClass: "project-gd-picture",
+          gdImgClassBig: "project-gd-picture-big",
+
           tam1: "/src/assets/graphic_design/angry_men1.JPG",
           tam1Alt: "Lorem",
           tam2: "/src/assets/graphic_design/angry_men2.JPG",
@@ -21,7 +23,10 @@ import LightboxImage from '../LightboxImage.vue';
           posterEntwurf2: "/src/assets/graphic_design/angry_men4.jpg",
           posterEntwurf2Alt: "Lorem",
           flyer: "/src/assets/graphic_design/theater_flyer.png",
-          flyerAlt: "Lorem"
+          flyerAlt: "Lorem",
+
+          communicating_doors1: "/src/assets/graphic_design/communicating_doors1.png",
+          communicating_doors1Alt: "Lorem"
         };
       },
 
@@ -37,6 +42,7 @@ import LightboxImage from '../LightboxImage.vue';
       </button>
       <div class="content-accordion">
         <div id="projects-tam-container">
+
         <h4>{{ $t("projects.graphicdesign.twelveAngryMen") }}</h4>
         
         <div class="tam-content-flexbox">
@@ -48,37 +54,49 @@ import LightboxImage from '../LightboxImage.vue';
             <p>{{ $t("projects.graphicdesign.tamtext5") }}</p>
             <p>{{ $t("projects.graphicdesign.tamtext6") }}</p>
           </div>
+
           <figure>
             <LightboxImage :imageClass="gdImgClass" :image="tam1" :altText="tam1Alt" />
             <figcaption> {{ $t("projects.graphicdesign.tam1under") }} </figcaption>
-         </figure>
-         <figure>
-          <LightboxImage :imageClass="gdImgClass" :image="tam2" :altText="tam2Alt" />
-          <figcaption> {{ $t("projects.graphicdesign.tam2under") }} </figcaption>
-        </figure>
+          </figure>
+
+          <figure>
+            <LightboxImage :imageClass="gdImgClass" :image="tam2" :altText="tam2Alt" />
+            <figcaption> {{ $t("projects.graphicdesign.tam2under") }} </figcaption>
+          </figure>
         </div>
+
         <div class="tam-content-flexbox">
           <figure>
             <LightboxImage :imageClass="gdImgClass" :image="posterEntwurf1" :altText="posterEntwurf1Alt" />
             <figcaption> {{ $t("projects.graphicdesign.tam3under") }} </figcaption>
           </figure>
+
           <figure>
             <LightboxImage :imageClass="gdImgClass" :image="poster" :altText="posterAlt" />
             <figcaption> {{ $t("projects.graphicdesign.tam4under") }} </figcaption>
           </figure>
-          <figure>
-            <LightboxImage :imageClass="gdImgClass" :image="posterEntwurf2" :altText="posterEntwurf2Alt" />
+
+           <figure>
+           <LightboxImage :imageClass="gdImgClass" :image="posterEntwurf2" :altText="posterEntwurf2Alt" />
             <figcaption> {{ $t("projects.graphicdesign.tam5under") }} </figcaption>
           </figure>
-          <figure>
+
+           <figure>
             <LightboxImage :imageClass="gdImgClass" :image="flyer" :altText="flyerAlt" />
           <figcaption> {{ $t("projects.graphicdesign.tam6under") }} </figcaption>
           </figure>
         </div>
+      </div>
+
+      <h4>{{ $t("projects.graphicdesign.communicatingDoors") }}</h4>
         
+        <div class="cd-content-flexbox">
+          <figure>
+            <LightboxImage :imageClass="gdImgClassBig" :image="communicating_doors1" :altText="communicating_doors1Alt" />
+            <figcaption> three parted poster design by Carina Huber</figcaption>
+          </figure>
         </div>
-        <h4>{{ $t("projects.graphicdesign.communicatingDoors") }}</h4>
-        <p>Lorem lorem</p>
       </div>
     </div>
 </template>
