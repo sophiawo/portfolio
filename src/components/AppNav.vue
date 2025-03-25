9c6c1625894d1b09d124e20a36db961c48afacfe
Sophia Wonka <sophia.wonka@hs-augsburg.de>
2025-03-25 22:44:10 +0100

    fixed navigation (w/o navAndFooter.scss)

--- src/components/AppNav.vue
+++ src/components/AppNav.vue
@@ -9,14 +9,14 @@ export default {
 </script>
 
 <template>
-  <div id="nav">
-    <ul class="list">
-      <li id="first">
+  <div id="nav" class="d-flex justify-content-between w-100 z-2">
+    <ul class="list w-100">
+      <li id="first" class="d-flex justify-content-between align-items-center">
           <span class="logo" id="logo_nav"><router-link to="/">W</router-link></span>
           <span class="name" id="name_nav"><router-link to="/">SOPHIA M. WONKA</router-link></span>
       </li>
       <li id="second">
-        <div id="links-nav" class="links">
+        <div id="links-nav" class="links d-flex justify-content">
           <router-link to="/">{{ $t("common.home") }}</router-link>
           <router-link to="/projects"> {{ $t("common.projects") }}</router-link>
           <router-link to="/about"> {{ $t("common.about") }}</router-link>
