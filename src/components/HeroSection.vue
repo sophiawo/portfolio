8e2616450b0920d78839c13ed7f82b7346b3059b
Sophia Wonka <sophia.wonka@hs-augsburg.de>
2025-03-25 22:05:48 +0100

    added bootstrap to herosection

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -11,24 +11,41 @@
 </script>
 
 <template>
-    <div id="hero-section">
-            <div class="gradient-background" aria-hidden="true">
-                <img id="grain" aria-hidden="true" src="../assets/hero_section/grain1.png">
-            </div>
-            <div id="hero-content">
-                <img id="me-picture" src="../assets/hero_section/itsme.png">
-                <h1 lang="de">SOPHIA M. WONKA</h1>
-                <p id="golden-ticket"> {{ $t("homepage.goldenticket") }}</p>
-                <div id="experience">
-                    <p class="experience1"> {{ $t("homepage.experiencein") }}</p>
-                    <p lang="en" class="experience2">UX/UI Design // Frontend Web Developement // Digital Accessability // Animation</p>
-                    <p class="experience3"> {{ $t("homepage.muchmore") }} </p>
+    <section id="hero-section" class="position-relative w-100 h-100">
+
+        <!-- Hero section background and grain-->
+        <div class="gradient-background w-100 position-absolute z-0" aria-hidden="true"></div>
+        <img id="grain" class="w-100 position-absolute z-0" aria-hidden="true" src="../assets/hero_section/grain1.png">
+            
+        <!-- Content -->
+        <div id="hero-content" class="container-fluid z-1 w-100">
+            <div class="row">
+
+                <!-- Portray -->
+                <div class="col-lg-5">
+                    <img id="me-picture" class="position-absolute" src="../assets/hero_section/itsme.png">
                 </div>
-                <div id="hero-button">
-                    <MyButton class="segment-button-black" @click="$router.push('projects')" :buttonTextKey="'homepage.heroButtonText'" />
+
+                <!-- Text & button -->
+                <div class="col-lg-7 hero-text">
+                    <h1 lang="de" class="position-absolute">SOPHIA M. WONKA</h1>
+                    <p id="golden-ticket" class="position-absolute"> {{ $t("homepage.goldenticket") }}</p>
+
+                    <!-- Experience -->
+                    <div id="experience" class="mt-4 position-absolute">
+                        <p class="experience1 pb-1"> {{ $t("homepage.experiencein") }}</p>
+                        <p lang="en" class="experience2 py-1">UX/UI Design // Frontend Web Developement // Digital Accessability // Animation</p>
+                        <p class="experience3 py-1"> {{ $t("homepage.muchmore") }} </p>
+                    </div>
+                
+                    <!-- Show me projects button-->
+                    <div id="hero-button" class="position-absolute">
+                        <MyButton class="segment-button-black" @click="$router.push('projects')" :buttonTextKey="'homepage.heroButtonText'" />
+                    </div>
                 </div>
             </div>
         </div>
+    </section>
 </template>
 
 <style scoped lang="scss">
